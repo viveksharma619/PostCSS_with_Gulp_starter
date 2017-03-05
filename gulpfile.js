b86bf4fec1d511f006f8f676f98d2f0862f6c25b
viveksharma619 <s.vivekblog@gmail.com>
2017-03-06 01:50:41 +0530

    Finished Gulp with PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webserver = require('gulp-webserver');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+var animation = require('postcss-animation');
+var cssnano = require('cssnano');
+
+var dest = 'build/css/';
+var src = 'src/postcss/';
+
+gulp.task('copyHtml', function(){
+    gulp.src('src/index.html')
+    .pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('css', function(){
+    gulp.src(src + '*.css')
+    .pipe(postcss([
+        precss(),
+        animation(),
+        autoprefixer(),
+        cssnano()
+    ]))
+    .on('error', gutil.log)
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('src/*.css',['css']);
+    gulp.watch('src/index.html');
+});
+
+gulp.task('webserver', function() {
+  gulp.src('build')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }));
+});
+
+gulp.task('default',['copyHtml', 'css', 'webserver' ,'watch']);
